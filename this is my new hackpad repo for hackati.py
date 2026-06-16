06cd4cc4e92900d375d9cfcef7852dd34f06b5a9
aayanalikhan09-hash <aayanalikhan09@gmail.com>
2026-06-16 19:40:07 -0400

    w hackatime setup

--- this is my new hackpad repo for hackati.py
+++ this is my new hackpad repo for hackati.py
@@ -1,1 +1,3 @@
 #this is my new hackpad repo for hackatime tracking idk how to get it to work otherwise
+#uhhh lets see if this gets it set up i really want this to work so i can get my hours logged for hackatime
+#now lets check again to make sure
